bb70bb4d15f0df5bc1ec4be96ed2bb252ac3127c
ChienChia000 <hcc7390@gmail.com>
2019-10-07 20:41:29 +0800

    move data from old github ChienChia00 to new github ChienChia000

---
+++ test.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int time,mana,y=10;
+	char c[4];
+	cout<<"C 9 1 3 8 6 4 2"<<endl;
+	while(1)
+	{
+		cin>>time>>mana>>c[0]>>c[1]>>c[2]>>c[3];
+//		if(c[0]=='9' || c[1]=='9' || c[2]=='9' || c[3]=='9')
+		
+			cout<<"1 "<<"9 10 1"<<endl;
+		
+//		if(c[0]=='4' || c[1]=='4' || c[2]=='4' || c[3]=='4')
+//		{
+			cout<<"1 "<<"C"<<" 5 21"<<endl;
+//			cout<<"0"<<endl;
+//		}
+		cout<<"1 "<<"4"<<" 16 16"<<endl;
+		cout<<"1 "<<"1"<<" 5 16"<<endl;
+		cout<<"1 "<<"3"<<" 5 17"<<endl;
+		cout<<"1 "<<"8"<<" 6 16"<<endl;
+		cout<<"1 "<<"6"<<" 6 17"<<endl;
+		cout<<"0"<<endl;
+	}
+
+	return 0;
+}
+
